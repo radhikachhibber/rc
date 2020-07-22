33c21af85996859bfc49cfe9adb9cc1837e8f548
radhikachhibber <46199978+radhikachhibber@users.noreply.github.com>
2020-07-22 13:06:16 -0400

    Updates

---
+++ precache-manifest.4118902595c31b6389ede2697626496b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1294015087daf63a288ce92a7dd3fc6",
+    "url": "/rc/index.html"
+  },
+  {
+    "revision": "e9b9e259d0e4c8458dad",
+    "url": "/rc/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "971fdf8e49ea6d15e11b",
+    "url": "/rc/static/css/main.8e52fe53.chunk.css"
+  },
+  {
+    "revision": "e9b9e259d0e4c8458dad",
+    "url": "/rc/static/js/2.4d744921.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rc/static/js/2.4d744921.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "971fdf8e49ea6d15e11b",
+    "url": "/rc/static/js/main.ab08f15d.chunk.js"
+  },
+  {
+    "revision": "d41ec1c2d0fe6ba8f31b",
+    "url": "/rc/static/js/runtime-main.3e57ea6c.js"
+  },
+  {
+    "revision": "1aa28afd0d673fb1e3688e4159a8f9c6",
+    "url": "/rc/static/media/JaynesCottages.1aa28afd.png"
+  },
+  {
+    "revision": "55af186e6c77f6a6f187402b1346d552",
+    "url": "/rc/static/media/Profile.55af186e.jpg"
+  },
+  {
+    "revision": "87927fa263bce92d3e113ef0a337bd8a",
+    "url": "/rc/static/media/SL.87927fa2.png"
+  },
+  {
+    "revision": "21772029f9b05051d7204706be7c1a29",
+    "url": "/rc/static/media/WBQuote.21772029.png"
+  },
+  {
+    "revision": "5ea6b95fe8997e7ac2ad022fb090c197",
+    "url": "/rc/static/media/mHSEQ.5ea6b95f.png"
+  }
+]);
